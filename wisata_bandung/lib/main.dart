636e074eecf134855ed312e40533e9008baf864e
umdp <ahmfarisi@gmail.com>
2023-11-27 09:35:29 +0700

    Update main_screen.dart

--- wisata_bandung/lib/main.dart
+++ wisata_bandung/lib/main.dart
@@ -11,6 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: DetailScreen());
+    return const MaterialApp(home: MainScreen());
   }
 }
--- wisata_bandung/lib/main_screen.dart
+++ wisata_bandung/lib/main_screen.dart
@@ -5,6 +5,35 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wisata Bandung'),
+      ),
+      body: Card(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('images/farm-house.jpg'),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Farm House Lembang',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Lembang')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
