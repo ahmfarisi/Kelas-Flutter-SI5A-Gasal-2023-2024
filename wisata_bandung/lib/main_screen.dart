389fd656bc6f5d364af2c9df5b0da65065a03a2f
umdp <ahmfarisi@gmail.com>
2023-11-29 12:16:40 +0700

    Update main_screen.dart

--- wisata_bandung/lib/main_screen.dart
+++ wisata_bandung/lib/main_screen.dart
@@ -6,40 +6,42 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wisata Bandung'),
-      ),
-      body: Card(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Image.asset('images/farm-house.jpg'),
-            ),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'Farm House Lembang',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text('Lembang')
-                  ],
-                ),
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: const Text('Wisata Bandung'),
         ),
-      ),
-    );
+        body: InkWell(
+          onTap: () {},
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Image.asset('images/farm-house.jpg'),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Farm House Lembang',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Lembang')
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
