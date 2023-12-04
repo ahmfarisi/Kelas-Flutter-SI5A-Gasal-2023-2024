f89dc00df7a655f1ecad4359bf9a6bccb44bf980
umdp <ahmfarisi@gmail.com>
2023-12-04 09:02:18 +0700

    Finish shows all tourism place to main screen

--- wisata_bandung/lib/main_screen.dart
+++ wisata_bandung/lib/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_bandung/detail_screen.dart';
+import 'package:wisata_bandung/model/tourism_place.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -9,39 +11,50 @@ class MainScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Wisata Bandung'),
         ),
-        body: InkWell(
-          onTap: () {},
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Image.asset('images/farm-house.jpg'),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'Farm House Lembang',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text('Lembang')
-                      ],
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            final TourismPlace place = tourismPlaceList[index];
+
+            return InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen();
+                }));
+              },
+              child: Card(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Image.asset(place.imageAsset),
                     ),
-                  ),
-                )
-              ],
-            ),
-          ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              place.name,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(place.location)
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+          itemCount: tourismPlaceList.length,
         ));
   }
 }
